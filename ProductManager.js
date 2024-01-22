38787c9e5e2a4979c4a67af15229e15db94038c0
Luis Hasenauer <101999089+LHasenauer@users.noreply.github.com>
2024-01-22 17:01:11 -0300

    Actualizacion

--- ProductManager.js
+++ ProductManager.js
@@ -1,93 +1,71 @@
 class ProductManager {
     constructor() {
-        this.products = [];
-        // this.nextProductId = 1;
+      this.products = [];
     }
-    generateId() {
-    // Esta función genera un ID único basado en la fecha actual.
-    return new Date().getTime().toString();
-    }
+
     getProducts() {
         return this.products;
     }
-    addProduct({ title, description, price, thumbnail, code, stock }) {
-      // Verificar si el código ya está en uso
-        const codeExists = this.products.some(product => product.code === code);
-        if (codeExists) {
-        throw new Error("El código de producto ya está en uso.");
-        }
-      // Generar un nuevo producto con un ID único
-    const newProduct = {
-        id: this.generateId(),
-        title,
-        description,
-        price,
-        thumbnail,
-        code,
-        stock,
-    };
-      // Agregar el nuevo producto al array de productos
-        this.products.push(newProduct);
-      // Devolver el nuevo producto
-        return newProduct;
+    static id = 0
+
+    addProduct(title, description, price, image, code, stock) {
+      ProductManager.id++
+      this.products.push({ title, description, price, image, code, stock, id:ProductManager.id})
     }
 
-    getProductById(productId) {
-    const product = this.products.find(product => product.id === productId);
-        if (!product) {
-        throw new Error("Producto no encontrado");
-        }
-        return product;
+    getProductById(id){
+      if(!this.products.find((producto) => producto.id === id)){
+        console.log("not found")
+      } else{
+        console.log("exist")
+      }
     }
 }
   // Crear una instancia de ProductManager
-  const productManager = new ProductManager();
+  const Productos = new ProductManager();
   
   // agrego productos
-  const newProduct = productManager.addProduct({
-    title: "racks",
-    description: "Rack de televisor E160",
+  Productos.addProduct({
+    title: "Mesa",
+    description: "Mesa 1.80x0.90m",
     price: 25000,
-    thumbnail: "Sin imagen",
+    image: "Sin imagen",
     code: "a1",
     stock: 25,
   });
-  const newProduct1 = productManager.addProduct({
-    title: "racks",
-    description: "Rack de televisor E180",
+  Productos.addProduct({
+    title: "Sillas",
+    description: "sillas de madera",
     price: 29000,
-    thumbnail: "Sin imagen",
+    image: "Sin imagen",
     code: "a2",
     stock: 10,
   });
-  const newProduct2 = productManager.addProduct({
+  Productos.addProduct({
     title: "Alacena",
     description: "alacena 3 puertas",
     price: 17000,
-    thumbnail: "Sin imagen",
+    image: "Sin imagen",
     code: "a3",
     stock: 15,
   });
-  const newProduct3 = productManager.addProduct({
-    title: "Alacena",
-    description: "alacena 4 puertas",
-    price: 24000,
-    thumbnail: "Sin imagen",
+  Productos.addProduct({
+    title: "Sillon",
+    description: "Sillon 3 cuerpos",
+    price: 350000,
+    image: "Sin imagen",
     code: "a4",
     stock: 7,
   });
-  console.log("Producto recién agregado:", newProduct);
-  console.log("Producto recién agregado:", newProduct1);
-  console.log("Producto recién agregado:", newProduct2);
-  console.log("Producto recién agregado:", newProduct3);
+  console.log("Producto recién agregado:", Productos.getProducts());
   
   // Agrego un producto y arroja error al agregar con mismo codigo
   try {
-    productManager.addProduct({
-      title: "racks",
-      description: "Rack de televisor A100",
-      price: 20000,
-      thumbnail: "Otra imagen",
+    Productos.addProduct({
+      title: "Zapatero",
+      description: "Zapatero standar",
+      price: 5000,
+      image: "Sin imagen",
       code: "a2",
       stock: 5,
     });
